2bcd4591f048cc69ea3a30fc33482f05684f18df
Mayowa Paul <lexypaul13@yahoo.com>
2023-09-01 22:49:24 -0400

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Alex Paul on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
